3efb26aa70037fb89f812689a89b237eb385d1a9
developersachinhack <sachinsaroj4246@gmail.com>
2025-05-30 17:46:50 +0530

    backend end point added

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://return-inventory-backend.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
