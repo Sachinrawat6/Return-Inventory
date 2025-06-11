148cf989b7bffb77d897b34f63213b561d667926
developersachinhack <sachinsaroj4246@gmail.com>
2025-06-11 12:55:30 +0530

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow hidden sm:block">
-      <div className="max-w-7xl mx-auto px-4">
+    <>
+    <nav className="bg-white  hidden sm:block">
+      <div className="container mx-auto px-4">
         <div className="flex justify-center h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <span className="text-xl font-bold text-gray-800"><span className="text-blue-400 ">Return</span> Inventory System</span>
+              <span className="text-xl font-bold text-gray-800"><span className="bg-[#222] rounded text-[#f1f1f1] py-1 px-4 ">Return</span> Inventory System</span>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                <NavLink
@@ -16,7 +17,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -28,7 +29,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -40,7 +41,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -52,7 +53,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -66,7 +67,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -78,7 +79,7 @@ const Navbar = () => {
                 className={({ isActive }) => 
                   `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
                     isActive 
-                      ? "border-blue-500 text-gray-900" 
+                      ? "border-black text-gray-900" 
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`
                 }
@@ -90,49 +91,9 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
-      {/* Mobile menu */}
-      <div className="hidden">
-        <div className="pt-2 pb-3 space-y-1">
-          <NavLink
-            to="/"
-            className={({ isActive }) => 
-              `block pl-3 pr-4 py-2 border-l-4 text-base font-medium ${
-                isActive 
-                  ? "bg-blue-50 border-blue-500 text-blue-700" 
-                  : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
-              }`
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/return-table-records"
-            className={({ isActive }) => 
-              `block pl-3 pr-4 py-2 border-l-4 text-base font-medium ${
-                isActive 
-                  ? "bg-blue-50 border-blue-500 text-blue-700" 
-                  : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
-              }`
-            }
-          >
-            Return Table Records
-          </NavLink>
-          <NavLink
-            to="/press-table-records"
-            className={({ isActive }) => 
-              `block pl-3 pr-4 py-2 border-l-4 text-base font-medium ${
-                isActive 
-                  ? "bg-blue-50 border-blue-500 text-blue-700" 
-                  : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
-              }`
-            }
-          >
-            Press Table Records
-          </NavLink>
-        </div>
-      </div>
     </nav>
+    <hr className="container mx-auto mt-2 text-gray-200" />
+    </>
   );
 };
 
